a3fda4f25c751a6e130211a4e34e01f6d1998ffa
Chien Tran <chientran@Chiens-MacBook-Pro.local>
2024-04-08 23:52:56 +0700

    0.1.0

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,26 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use open::that;
+use rusty_ytdl::{Video, VideoOptions, VideoQuality, VideoSearchOptions};
+
+#[tauri::command]
+async fn download(url: &str) -> Result<(), String> {
+    let video_options = VideoOptions {
+        quality: VideoQuality::HighestVideo,
+        filter: VideoSearchOptions::Video,
+        ..Default::default()
+    };
+    let video = Video::new_with_options(url, video_options).map_err(|e| e.to_string())?;
+    let info = video.get_info().await.map_err(|e| e.to_string())?;
+    let url = info.formats.first().unwrap().url.clone();
+    let _ = that(url);
+    Ok(())
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![download])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,55 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let url = '';
+	let loading = false;
+	let result = '';
+	let error = '';
+
+	const download = async () => {
+		loading = true;
+		error = '';
+		try {
+			await invoke('download', { url });
+		} catch (e: any) {
+			error = e;
+		}
+		loading = false;
+	};
+</script>
+
 <h1>XTuber</h1>
 <p>Simple YouTube Downloader</p>
 
-<form>
+<div>
 	<label>
 		URL:
-		<input name="url" placeholder="https://youtube.com/..." required />
-		<button>Load</button>
+		<input name="url" placeholder="https://youtube.com/..." bind:value={url} disabled={loading} />
+		<button on:click={download} disabled={loading}>
+			{#if loading}
+				Processing...
+			{:else}
+				Download
+			{/if}
+		</button>
 	</label>
-</form>
+</div>
+
+{#if error}
+	<span style="color: red">{error}</span>
+{/if}
+
+<span>{result}</span>
+<br />
+&copy; 2024 <a href="https://chientrm.com" target="_blank">chientrm.com</a>
+
+<style>
+	h1,
+	p,
+	div,
+	span {
+		user-select: none;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+	}
+</style>
