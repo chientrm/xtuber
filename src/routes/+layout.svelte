852ef28d984ed5dbd5af4ce2d8630c19679a7753
chientrm <chientrm@gmail.com>
2024-04-13 08:11:56 +0000

    f

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,33 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import icon from '$lib/icon.png';
 	import BxDonateBlood from '~icons/bx/donate-blood';
-	import MdiGithub from '~icons/mdi/github';
-	import RiTwitterXFill from '~icons/ri/twitter-x-fill';
 	import '../app.pcss';
+	let value = '';
+	const load = () => {
+		const text = value.trim();
+		if (text.length) {
+			const url = new URL(text);
+			const v = url.searchParams.get('v');
+			if (v) {
+				goto(`/url?v=${v}`);
+			}
+		}
+	};
 </script>
 
 <header
 	class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
-	<div class="container flex h-14 w-full items-center">
+	<div class="container flex h-14 w-full items-center gap-2">
 		<a href="/" class="mr-6 flex items-center space-x-2">
 			<img src={icon} alt="icon" class="h-6 w-6" />
 			<span class="font-bold">XTuber</span>
 		</a>
-		<div class="grow" />
-		<Button variant="ghost" class="mr-2 w-9 px-0" href="https://x.com/realchientrm" target="_blank">
-			<RiTwitterXFill class="h-[1.2rem] w-[1.2rem]" />
-			<span class="sr-only">X</span>
-		</Button>
-		<Button
-			variant="ghost"
-			class="mr-2 w-9 px-0"
-			href="https://github.com/chientrm/xtuber"
-			target="_blank"
-		>
-			<MdiGithub class="h-[1.2rem] w-[1.2rem]" />
-			<span class="sr-only">GitHub Repo</span>
-		</Button>
+		<Input type="url" placeholder="https://youtube.com/..." class="grow" bind:value />
+		<Button on:click={load}>Load</Button>
 		<Button variant="ghost" href="https://www.buymeacoffee.com/chientrm" target="_blank">
 			<BxDonateBlood class="h-[1.2rem] w-[1.2rem]" />
 			<span>Donate</span>
@@ -36,6 +35,4 @@
 	</div>
 </header>
 
-<div class="p-4">
-	<slot />
-</div>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,24 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
-	import { Input } from '$lib/components/ui/input';
-	import { Label } from '$lib/components/ui/label';
-	let value = '';
-	const load = () => {
-		const tmp = value.trim();
-		if (tmp.length) {
-			const url = new URL(tmp);
-			const v = url.searchParams.get('v');
-			if (v) {
-				goto(`/url?v=${v}`);
-			}
-		}
-	};
+	import MdiGithub from '~icons/mdi/github';
+	import RiTwitterXFill from '~icons/ri/twitter-x-fill';
 </script>
 
-<Card.Root class="max-w-[480px]">
+<Card.Root class="m-4 max-w-[480px]">
 	<Card.Header>
-		<Card.Title>Download</Card.Title>
+		<Card.Title>XTuber</Card.Title>
 		<Card.Description>Simple YouTube Downloader.</Card.Description>
 	</Card.Header>
 	<Card.Content>
-		<Label for="url">Url</Label>
-		<Input id="url" placeholder="https://youtube.com/..." bind:value />
+		<Button variant="secondary" href="https://x.com/realchientrm" target="_blank">
+			<span>Profile</span>
+			<RiTwitterXFill class="ml-2 h-4 w-4" />
+		</Button>
+		<Button variant="secondary" href="https://github.com/chientrm/xtuber" target="_blank">
+			<span>GitHub</span>
+			<MdiGithub class="ml-2 h-4 w-4" />
+		</Button>
 	</Card.Content>
-	<Card.Footer class="justify-end">
-		<Button on:click={load}>Load</Button>
-	</Card.Footer>
+	<Card.Footer class="justify-end"></Card.Footer>
 </Card.Root>
